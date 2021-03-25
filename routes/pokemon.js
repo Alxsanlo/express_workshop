11283a174fba45dc3ef79f3234b094615ae064b9
Emilio Alejandro Sánchez López <Alexsanlo90@hotmail.com>
2021-03-25 16:59:41 -0600

    Se quitó la librería body parser ya que express tiene una desde la versión 4.16 por defecto, y se reemplazaron las respuestas por un json con un código y un mensaje para hacerlo más legible para el usuario

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,23 +4,22 @@ const pokemon = express.Router()
 const db = require('../config/database');
 
 pokemon.post("/", (req, res, next) =>{
-	return res.status(200).send(req.body);
+	return res.status(200).json(req.body);
 });
 
 pokemon.get('/', async (req, res, next) =>{
 	const pkmn = await db.query("SELECT * FROM pokemon");
-	console.log(pkmn);
-	return res.status(200).send(pkmn);
+	return res.status(200).json({code: 1, message: pkmn});
 });
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) =>{
-	const pkmn = await db.query("SELECT * FROM pokemon");
-	const id = req.params.id - 1;
-	if(id >= 0 && id <= 150){
-		return res.status(200).send(pkmn[req.params.id - 1]);
+	const id = req.params.id;
+	if(id >= 0 && id <= 722){
+		const pkmn = await db.query("SELECT * FROM WHERE pok_id" + id + ";");
+		return res.status(200).json({code: 1, message: pkmn});
 	}
 	
-	return res.status(404).send("Pokemon no encontrado");
+	return res.status(404).send({code: 404, message: "Pokemon no encontrado"});
 	
 });
 
@@ -39,7 +38,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) =>{
 
 	(pkmn.length > 0) ? 
 		res.status(200).send(pkmn) : 
-		res.status(404).send("Pokemon no encontrado");
+		res.status(404).send({code: 404, message: "Pokemon no encontrado"});
 });
 
 
