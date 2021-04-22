90644a472b03100d080689dde0f0ea38676dd36f
Emilio Alejandro Sánchez López <Alexsanlo90@hotmail.com>
2021-04-21 19:08:37 -0500

    nueva ruta

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
-const pokemon = require('./routes/pokemon')
+const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
+
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -26,6 +28,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/pokemon", pokemon);
+app.use("/user", user);
 
 app.use((req, res, next) =>{
 	return res.status(404).json({code: 404, message: "URL no encontrada"});
